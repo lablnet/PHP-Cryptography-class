65e49a385c5487a09342382459a6c7972ba10a4f
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-26 06:39:41 +0000

    Apply fixes from StyleCI

--- classes/Cryptography.php
+++ classes/Cryptography.php
@@ -1,53 +1,61 @@
-<?php 
-class Cryptography{      
+<?php
 
-	private static $secret_key = 'gsdgsg423b523b5432bjbjm24vbjn2hv';
-	const CIPHER_16 = 'AES-128-CBC';
+class Cryptography
+{
+    private static $secret_key = 'gsdgsg423b523b5432bjbjm24vbjn2hv';
+    const CIPHER_16 = 'AES-128-CBC';
     const CIPHER_32 = 'AES-256-CBC';
 
-	public static function encrypt($str,$cl=32){
-		return static::encyptedDecypted('encrypt',$str,$cl);
-	}
-	public static function decrypt($str,$cl=32){
-		return static::encyptedDecypted('decrypt',$str,$cl);
-	}	
-	public static function encyptedDecypted($action,$str,$cl){
+    public static function encrypt($str, $cl = 32)
+    {
+        return static::encyptedDecypted('encrypt', $str, $cl);
+    }
 
-		$cl = (int) $cl;
+    public static function decrypt($str, $cl = 32)
+    {
+        return static::encyptedDecypted('decrypt', $str, $cl);
+    }
 
-		if($cl === 16){
-			$cipher = static::CIPHER_16;
-			$length = 16;
-		}elseif($cl === 32){
-			$cipher = static::CIPHER_32;
-			$length = 32;
-		}else{
-			throw new Exception("Error Processing Request", 1);
-			
-		}
-		$iv =  $iv = substr(hash('sha256',static:: $secret_key), 0, 16);
-		$key = hash('sha512', static::$secret_key);
-	    if ( $action == 'encrypt' ) {
-	        $output = openssl_encrypt($str, $cipher, $key, 0, $iv);
-	        $output = base64_encode($output);
-	        $output = static::securesalts($length).$output.static::securesalts($length);
-	    } else if( $action == 'decrypt' ) {
-	    	$str = $text = substr($str, $length, -$length);
-	        $output = openssl_decrypt(base64_decode($str), $cipher, $key, 0, $iv);
-	    }
-	    return $output;
-	}
-	private static function securesalts($length){
-		if(is_int($length) && $length >= 5){
-			$chars =  array_merge(range(0,9), range('a', 'z'),range('A', 'Z'));
-			$stringlength = count( $chars  ); //Used Count because its array now
-			$randomString = '';
-			for ( $i = 0; $i < $length; $i++ ) {
-				$randomString .= $chars[rand( 0, $stringlength - 1 )];				
-			}			
-			return $randomString;			
-		}else{
-			return false;
-		}
-	}	
+    public static function encyptedDecypted($action, $str, $cl)
+    {
+        $cl = (int) $cl;
+
+        if ($cl === 16) {
+            $cipher = static::CIPHER_16;
+            $length = 16;
+        } elseif ($cl === 32) {
+            $cipher = static::CIPHER_32;
+            $length = 32;
+        } else {
+            throw new Exception('Error Processing Request', 1);
+        }
+        $iv = $iv = substr(hash('sha256', static:: $secret_key), 0, 16);
+        $key = hash('sha512', static::$secret_key);
+        if ($action == 'encrypt') {
+            $output = openssl_encrypt($str, $cipher, $key, 0, $iv);
+            $output = base64_encode($output);
+            $output = static::securesalts($length).$output.static::securesalts($length);
+        } elseif ($action == 'decrypt') {
+            $str = $text = substr($str, $length, -$length);
+            $output = openssl_decrypt(base64_decode($str), $cipher, $key, 0, $iv);
+        }
+
+        return $output;
+    }
+
+    private static function securesalts($length)
+    {
+        if (is_int($length) && $length >= 5) {
+            $chars = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z'));
+            $stringlength = count($chars); //Used Count because its array now
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $chars[rand(0, $stringlength - 1)];
+            }
+
+            return $randomString;
+        } else {
+            return false;
+        }
+    }
 }
